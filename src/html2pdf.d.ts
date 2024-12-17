4f5951beb91d3494d9c8eaa9b05e7a0c908f98fd
Lakshay Goyal <lakshaygoyal201@gmail.com>
2024-12-17 19:26:39 +0530

    change the rules in eslint.config.mjs file

--- src/html2pdf.d.ts
+++ src/html2pdf.d.ts
@@ -1,19 +1,4 @@
 declare module 'html2pdf.js' {
-  interface Html2PdfOptions {
-    margin?: number | [number, number, number, number];
-    filename?: string;
-    image?: { type: string; quality: number };
-    html2canvas?: any; // Optional: Specify a library type if needed
-    jsPDF?: { unit: string; format: string; orientation: string };
+    const html2pdf: any;
+    export default html2pdf;
   }
-
-  const html2pdf: {
-    (): void;
-    from: (element: HTMLElement | string) => {
-      save: () => void;
-      set: (options: Html2PdfOptions) => any;
-    };
-  };
-
-  export default html2pdf;
-}
